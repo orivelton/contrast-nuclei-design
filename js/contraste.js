dba24f04b0328e5d9e48fed78e6dcc3de36cb619
Orivelton Cesar da Conceição <orivelton.cesar@keyrus.com.br>
2016-11-17 17:44:48 -0200

    Adição de explicação do script

--- js/contraste.js
+++ js/contraste.js
@@ -3,6 +3,7 @@ Artigo : Contraste de cores
 Autor : Orivelton Cesar - www.orivelton.com.br
 */
 
+// Verificação do cookie
 var c_name_contrasteBranco = document.cookie.indexOf('contrasteBranco');
 var c_name_contrastePreto = document.cookie.indexOf('contrastePreto');
 var c_name_contrasteAzul = document.cookie.indexOf('contrasteAzul');
@@ -16,15 +17,13 @@ if (c_name_contrasteBranco == 0) {
 } else {
   cookieTrue = '';
 }
-
-
-
+//Function no load da página
 contraste(cookieTrue);
 
-
-
+//Value passado pelo onclick do link (valueIndex)
 function contraste (valueIndex) {
   var setId = '';
+  //Verificação de qual contraste foi selecionado
   switch (valueIndex) {
     case 1:
     setId = 'contrasteBranco'
@@ -38,7 +37,8 @@ function contraste (valueIndex) {
     default:
     setId = ''
   }
+  // set do ID do contraste escolhido no body
   document.getElementsByTagName("body")[0].setAttribute("id", setId);
+  // Guardando o cookie do contraste
   document.cookie = "contraste=" + setId + "";
-
 }
