811a03c76c3ce92f9e614af6683e3eb8d2d3f9cb
Orivelton Cesar da Conceição <orivelton.cesar@keyrus.com.br>
2016-11-17 15:23:32 -0200

    Atualização

--- js/contraste.js
+++ js/contraste.js
@@ -2,34 +2,20 @@
 Artigo : Contraste de cores
 Autor : Orivelton Cesar - www.orivelton.com.br
 */
-
 function contraste (valueIndex) {
-var setId = '';
-switch (valueIndex) {
-  case 1:
-  setId = 'contrasteBranco'
-  break;
-  case 2:
-  setId = 'contrastePreto'
-  break;
-  case 3:
-  setId = 'contrasteAzul'
-  break;
-  default:
-  setId = ''
-}
-
-
-document.getElementsByTagName("body")[0].setAttribute("id", setId);
-
-
-  // if (value == 'contrasteAzul') {
-  //
-  // } else if (value == 'contrasteBranco') {
-  //   document.getElementsByTagName("body")[0].setAttribute("id", "contrasteBranco");
-  // } else if (value == 'contrastePreto') {
-  //   document.getElementsByTagName("body")[0].setAttribute("id", "contrastePreto");
-  // } else {
-  //   document.getElementsByTagName("body")[0].removeAttribute("id");
-  // }
+  var setId = '';
+  switch (valueIndex) {
+    case 1:
+    setId = 'contrasteBranco'
+    break;
+    case 2:
+    setId = 'contrastePreto'
+    break;
+    case 3:
+    setId = 'contrasteAzul'
+    break;
+    default:
+    setId = ''
+  }
+  document.getElementsByTagName("body")[0].setAttribute("id", setId);
 }
